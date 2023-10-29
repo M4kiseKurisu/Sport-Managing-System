87d2ede86e07f7a0b21591528a8eea8ebbb63a3d
陈俊华 <21373307@buaa.edu.cn>
2023-10-29 12:48:25 +0800

    实现团体部分相关接口

--- backend/api/views/user.py
+++ backend/api/views/user.py
@@ -1,3 +1,6 @@
+"""
+管理用户实体
+"""
 import json
 import random
 from django.http import JsonResponse
@@ -19,34 +22,34 @@ def login(request):
         password = request.GET.get('password')
         user = User.objects.filter(account=account, password=password).first()
         if user:
-            return JsonResponse({"msg": '登录成功', "res": True, "id": user.uid, "user_name": user.user_name})
+            return JsonResponse({"msg": '登录成功', "res": True, "uid": user.uid, "user_name": user.user_name})
         else:
-            return JsonResponse({"msg": '登录失败', "res": False, "id": '000000', "user_name": None})
+            return JsonResponse({"msg": '登录失败', "res": False, "uid": None, "user_name": None})
     else:
         return JsonResponse({"msg": "请求方式有误"})
 
 
 def register(request):
     if request.method == 'POST':
-        data = json.loads(request.body)
+        data: dict = json.loads(request.body)
         account = data.get('account')
         phone_number = data.get("phone_number")
         email = data.get("email")
         if User.objects.filter(account=account).first():
-            return JsonResponse({"msg": "账号已被注册", "id": "", "user_name": ""})
-        elif User.objects.filter(Q(phone_number=phone_number)|Q(email=email)).first():
-            return JsonResponse({"msg": "电话或邮箱已被注册", "id": "", "user_name": ""})
+            return JsonResponse({"msg": "账号已被注册", "uid": "", "user_name": ""})
+        elif User.objects.filter(Q(phone_number=phone_number) | Q(email=email)).first():
+            return JsonResponse({"msg": "电话或邮箱已被注册", "uid": "", "user_name": ""})
         else:
             uid = genid()
             password = data.get('password')
             user_name = data.get('user_name')
             user_age = data.get("user_age")
             user_gender = data.get("user_gender")
-            ps = data.get("personal_signature")
+            user_signature = data.get("user_signature")
             new_user = User(uid=uid, account=account, password=password, user_name=user_name,
                             user_age=user_age, user_gender=user_gender, phone_number=phone_number,
-                            email=email, personal_signature=ps)
+                            email=email, user_signature=user_signature)
             new_user.save()
-            return JsonResponse({"msg": "注册成功", "id": uid, "user_name": user_name})
+            return JsonResponse({"msg": "注册成功", "uid": uid, "user_name": user_name})
     else:
         return JsonResponse({"msg": "请求方式有误"})
