a24bd00976af934585560936340e328cc8a8339c
M4kiseKurisu <21373343@buaa.edu.cn>
2023-11-18 21:26:11 +0800

    finish borrowpage

--- frontend/src/components/MainPage/MainPageContainer.vue
+++ frontend/src/components/MainPage/MainPageContainer.vue
@@ -8,11 +8,8 @@
         </el-aside>
         <el-main>
           <!-- <UserInformation /> -->
-<<<<<<< HEAD
           <!-- <ActivityInformation /> -->
-=======
-          <GroupInformation />
->>>>>>> e72df933f9e9f24a2003371097af1e763859b4da
+          <!-- <GroupInformation /> -->
           <!-- <EquipmentBorrow /> -->
           <!-- <EBorrowMain /> -->
           <router-view></router-view>
@@ -28,13 +25,10 @@ import Sidebar from './SideBar/Sidebar.vue'; // 请替换为实际的路径
 import UserInformation from './Page/UserInformation.vue';
 import ActivityInformation from './Page/ActivityInformation.vue';
 import EquipmentBorrow from './Page/EquipmentBorrow.vue'
-<<<<<<< HEAD
 import EBorrowMain from './Page/E-Borrow/EBorrowMain.vue'
-=======
 import FriendInformation from './Page/FriendInformation.vue';
 import GroupInformation from './Page/GroupInformation.vue'
 
->>>>>>> e72df933f9e9f24a2003371097af1e763859b4da
 
 export default {
   components: {
@@ -42,14 +36,10 @@ export default {
     UserInformation,
     ActivityInformation,
     EquipmentBorrow,
-<<<<<<< HEAD
     EBorrowMain,
-  },
-=======
     FriendInformation,
     GroupInformation,
     UserInformation
-},
->>>>>>> e72df933f9e9f24a2003371097af1e763859b4da
+  },
 };
 </script>
--- frontend/src/components/MainPage/Page/E-Borrow/EBorrowMain.vue
+++ frontend/src/components/MainPage/Page/E-Borrow/EBorrowMain.vue
@@ -1,7 +1,15 @@
 <template>
-    <el-menu mode="horizontal" router="true">
+    <el-menu :default-active="activeIndex" mode="horizontal" router="true">
         <el-menu-item index="/Borrow_Equipment/Equipment_Information">器材租借</el-menu-item>
-        <el-menu-item index="/Borrow_Equipment/Personal_Borrow">我的借还情况</el-menu-item>
+        <el-sub-menu>
+            <template #title>
+                <div class="fontsize">
+                    我的借还情况
+                </div>
+            </template>
+            <el-menu-item index="/Borrow_Equipment/Borrow/Personal">个人借出</el-menu-item>
+            <el-menu-item index="/Borrow_Equipment/Borrow/Group">团体借出</el-menu-item>
+        </el-sub-menu>
     </el-menu>
     <router-view></router-view>
 </template>
@@ -10,8 +18,18 @@
 export default {
     data() {
         return {
-
+            activeIndex: ""
         }
     }
 }
 </script>
+
+<style scoped>
+.el-menu-item {
+    font-size: 16px;
+}
+
+.fontsize {
+    font-size: 16px;
+}
+</style>
