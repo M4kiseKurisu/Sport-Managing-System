d4cfbd7460e79f0aacf579cc03a1fa3c490976ad
M4kiseKurisu <21373343@buaa.edu.cn>
2023-11-29 15:31:27 +0800

    暂存

---
+++ frontend/src/components/MainPage/Page/Activity/ActivityBefore.vue
@@ -0,0 +1,29 @@
+<template>
+    <!-- 界面标题 -->
+    <div class="title">
+        <div>活动列表</div>
+    </div>
+
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style scoped>
+.title {
+    font-size: 20px;
+    margin-bottom: 16px;
+    margin-top: 16px;
+}
+</style>
---
+++ frontend/src/components/MainPage/Page/Activity/ActivityMain.vue
@@ -0,0 +1,27 @@
+<template>
+    <el-menu :default-active="activeIndex" mode="horizontal" router="true">
+        <el-menu-item index="/Page/Activity_Information/Show">热门活动</el-menu-item>
+        <el-menu-item index="/Page/Activity_Information/Before">过往活动</el-menu-item>
+    </el-menu>
+    <router-view></router-view>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            activeIndex: "/Activity_Information/Show"
+        }
+    }
+}
+</script>
+
+<style scoped>
+.el-menu-item {
+    font-size: 16px;
+}
+
+.fontsize {
+    font-size: 16px;
+}
+</style>
